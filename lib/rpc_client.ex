0f99b58ecf600c3578531a0795063d1af927930d
Laetitia <l.ohayon@bevaday.com>
2021-02-18 15:08:36 +0200

    add call! to RpcClient

--- lib/rpc_client.ex
+++ lib/rpc_client.ex
@@ -1,6 +1,14 @@
 defmodule AntlUtilsElixir.RpcClient do
   @moduledoc "A tiny Rpc Client"
 
+  defmodule NodeDownError do
+    defexception [:message]
+  end
+
+  defmodule BadCallError do
+    defexception [:message]
+  end
+
   require Logger
 
   @doc """
@@ -19,11 +27,34 @@ defmodule AntlUtilsElixir.RpcClient do
   @spec call(atom, atom, atom, [any]) :: any
   def call(remote_node, module, function_name, attributes)
       when is_atom(module) and is_atom(function_name) and is_list(attributes) do
+    call_and_log(remote_node, module, function_name, attributes)
+    |> process_response()
+  end
+
+  @doc """
+  Similar to `c:call/4` but raises if the call failed.
+
+  ## Examples
+
+      iex> AntlUtilsElixir.RpcClient.call!(:node, Kernel, :+, [1, 2])
+      3
+
+      iex> AntlUtilsElixir.RpcClient.call!(:node, Kernel, :undefined_function, [1, 2])
+      (** RpcError) got: "failure reason"
+
+  """
+  @spec call!(atom, atom, atom, [any]) :: any
+  def call!(remote_node, module, function_name, attributes) do
+    call_and_log(remote_node, module, function_name, attributes)
+    |> process_response!()
+  end
+
+  defp call_and_log(remote_node, module, function_name, attributes)
+       when is_atom(module) and is_atom(function_name) and is_list(attributes) do
     Logger.debug("#{remote_node} - #{module} - #{function_name} - #{inspect(attributes)}")
 
     :rpc.call(remote_node, module, function_name, attributes)
     |> log_response()
-    |> process_response()
   end
 
   defp log_response(response) do
@@ -37,4 +68,11 @@ defmodule AntlUtilsElixir.RpcClient do
   defp process_response({:badrpc, _error}), do: {:error, :failed}
 
   defp process_response(response), do: response
+
+  defp process_response!({:badrpc, :nodedown}), do: raise(AntlUtilsElixir.RpcClient.NodeDownError)
+
+  defp process_response!({:badrpc, error}),
+    do: raise(AntlUtilsElixir.RpcClient.BadCallError, inspect(error))
+
+  defp process_response!(response), do: response
 end
--- test/rpc_client_test.exs
+++ test/rpc_client_test.exs
@@ -4,38 +4,87 @@ defmodule AntlUtilsElixir.RpcClientTest do
   import ExUnit.CaptureLog
 
   alias AntlUtilsElixir.RpcClient
+  alias AntlUtilsElixir.RpcClient.{NodeDownError, BadCallError}
 
   @node :"node1@127.0.0.1"
 
-  test "when node is down" do
-    assert {:error, :nodedown} = RpcClient.call(:unexisting_node, Kernel, :+, [1, 2])
+  describe "call/4" do
+    test "when node is down" do
+      assert {:error, :nodedown} = RpcClient.call(:unexisting_node, Kernel, :+, [1, 2])
 
-    assert capture_log(fn -> RpcClient.call(:unexisting_node, Kernel, :+, [1, 2]) end) =~
-             "node - Elixir.Kernel - + - [1, 2]"
+      assert capture_log(fn -> RpcClient.call(:unexisting_node, Kernel, :+, [1, 2]) end) =~
+               "node - Elixir.Kernel - + - [1, 2]"
 
-    assert capture_log(fn -> RpcClient.call(:unexisting_node, Kernel, :+, [1, 2]) end) =~
-             "{:badrpc, :nodedown}"
+      assert capture_log(fn -> RpcClient.call(:unexisting_node, Kernel, :+, [1, 2]) end) =~
+               "{:badrpc, :nodedown}"
+    end
+
+    @tag :distributed
+    test "when node is up and the call failed" do
+      assert {:error, :failed} = RpcClient.call(@node, Kernel, :unexisting_function, [:atom])
+
+      assert capture_log(fn -> RpcClient.call(@node, Kernel, :to_string, [:atom]) end) =~
+               "#{@node} - Elixir.Kernel - to_string - [:atom]"
+
+      assert capture_log(fn -> RpcClient.call(@node, Kernel, :to_string, [:atom]) end) =~
+               "{:badrpc,"
+    end
+
+    @tag :distributed
+    test "when node is up " do
+      assert 3 = RpcClient.call(@node, Kernel, :+, [1, 2])
+
+      assert capture_log(fn -> RpcClient.call(@node, Kernel, :+, [1, 2]) end) =~
+               "#{@node} - Elixir.Kernel - + - [1, 2]"
+
+      assert capture_log(fn -> RpcClient.call(@node, Kernel, :+, [1, 2]) end) =~
+               "3"
+    end
   end
 
-  @tag :distributed
-  test "when node is up and the call failed" do
-    assert {:error, :failed} = RpcClient.call(@node, Kernel, :unexisting_function, [:atom])
+  describe "call!/4" do
+    test "when node is down" do
+      assert_raise NodeDownError, fn ->
+        RpcClient.call!(:unexisting_node, Kernel, :+, [1, 2])
+      end
 
-    assert capture_log(fn -> RpcClient.call(@node, Kernel, :to_string, [:atom]) end) =~
-             "#{@node} - Elixir.Kernel - to_string - [:atom]"
+      assert capture_log(fn -> RpcClient.call!(:unexisting_node, Kernel, :+, [1, 2]) end) =~
+               "node - Elixir.Kernel - + - [1, 2]"
 
-    assert capture_log(fn -> RpcClient.call(@node, Kernel, :to_string, [:atom]) end) =~
-             "{:badrpc,"
-  end
+      assert capture_log(fn -> RpcClient.call!(:unexisting_node, Kernel, :+, [1, 2]) end) =~
+               "{:badrpc, :nodedown}"
+    end
 
-  @tag :distributed
-  test "when node is up " do
-    assert 3 = RpcClient.call(@node, Kernel, :+, [1, 2])
+    @tag :distributed
+    test "when node is up and the call failed" do
+      assert_raise BadCallError, ~r/{:EXIT/, fn ->
+        RpcClient.call!(@node, Kernel, :unexisting_function, [:atom])
+      end
 
-    assert capture_log(fn -> RpcClient.call(@node, Kernel, :+, [1, 2]) end) =~
-             "#{@node} - Elixir.Kernel - + - [1, 2]"
+      assert capture_log(fn ->
+               Task.start(fn -> RpcClient.call!(@node, Kernel, :to_string, [:atom]) end)
+             end) =~
+               "#{@node} - Elixir.Kernel - to_string - [:atom]"
 
-    assert capture_log(fn -> RpcClient.call(@node, Kernel, :+, [1, 2]) end) =~
-             "3"
+      assert capture_log(fn ->
+               Task.start(fn -> RpcClient.call!(@node, Kernel, :to_string, [:atom]) end)
+             end) =~
+               "{:badrpc,"
+    end
+
+    @tag :distributed
+    test "when node is up " do
+      assert 3 = RpcClient.call!(@node, Kernel, :+, [1, 2])
+
+      assert capture_log(fn ->
+               Task.start(fn -> RpcClient.call!(@node, Kernel, :+, [1, 2]) end)
+             end) =~
+               "#{@node} - Elixir.Kernel - + - [1, 2]"
+
+      assert capture_log(fn ->
+               Task.start(fn -> RpcClient.call!(@node, Kernel, :+, [1, 2]) end)
+             end) =~
+               "3"
+    end
   end
 end
