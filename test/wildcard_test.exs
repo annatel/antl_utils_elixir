5ba0e236a3b9f59687bdffb49e6682476e116af3
Michael Twito <michael.twito@annatel.net>
2021-12-02 11:27:52 +0200

    wildcard changed

--- test/wildcard_test.exs
+++ test/wildcard_test.exs
@@ -4,82 +4,56 @@ defmodule AntlUtilsElixir.WildcardTest do
   alias AntlUtilsElixir.Wildcard
 
   @separator "."
-  @wildcard_char "*"
+  @wildcard_char "+"
 
   describe "match?/4" do
-    test "match all" do
-      assert Wildcard.match?(@wildcard_char, "a.b.c", @separator, @wildcard_char)
+    test "when the expr match the pattern" do
+      assert Wildcard.match?("+.bb.cc", "aa.bb.cc", @separator, @wildcard_char)
+      assert Wildcard.match?("aa.+.cc", "aa.bb.cc", @separator, @wildcard_char)
+      assert Wildcard.match?("aa.bb.+", "aa.bb.cc", @separator, @wildcard_char)
+      assert Wildcard.match?("aa.+.+", "aa.bb.cc", @separator, @wildcard_char)
+      assert Wildcard.match?("+.+.cc", "aa.bb.cc", @separator, @wildcard_char)
+      assert Wildcard.match?("+.+.+", "aa.bb.cc", @separator, @wildcard_char)
     end
 
-    test "with one wildcard" do
-      assert Wildcard.match?("a.*", "a.b.c", @separator, @wildcard_char)
-      refute Wildcard.match?("a.*", "b.b.c", @separator, @wildcard_char)
-      refute Wildcard.match?("a.*", "ab.b", @separator, @wildcard_char)
-      assert Wildcard.match?("*.c", "a.b.c", @separator, @wildcard_char)
-      refute Wildcard.match?("*.c", "a.b.b", @separator, @wildcard_char)
-      refute Wildcard.match?("*.c", "e.fc", @separator, @wildcard_char)
+    test "when the expr number of topics doesnt match the pattern number of topics " do
+      refute Wildcard.match?("aa.+", "aa.bb.cc", @separator, @wildcard_char)
+      refute Wildcard.match?("aa.+", "aa", @separator, @wildcard_char)
     end
 
-    test "with two non-adjacent anchors" do
-      assert Wildcard.match?("*.b.*.d.*", "a.b.c.d.e", @separator, @wildcard_char)
-      refute Wildcard.match?("*.b.*.d.*", "a.bb.c.dd.e", @separator, @wildcard_char)
-      assert Wildcard.match?("*.b.*.d.*", "a.b.c.c.d.e.f", @separator, @wildcard_char)
-    end
-
-    test "with three non-adjacent anchors" do
-      assert Wildcard.match?("a.*.b.*.c", "a.d.e.b.f.c", @separator, @wildcard_char)
-      refute Wildcard.match?("a.*.b.*.c", "a.b.c", @separator, @wildcard_char)
-    end
-
-    test "when the pattern does not contain wilcards, return true only for exact match " do
-      assert Wildcard.match?("a", "a", @separator, @wildcard_char)
-      refute Wildcard.match?("a", "a.b", @separator, @wildcard_char)
-    end
-
-    test "when the pattern anchor instance is at the end of the string" do
-      assert Wildcard.match?("a.*.b.c", "a.d.e.f.b.c", @separator, @wildcard_char)
-      refute Wildcard.match?("a.*.b.c", "a.d.e.b.f.c", @separator, @wildcard_char)
-      refute Wildcard.match?("a.*.b.c", "b.c", @separator, @wildcard_char)
-    end
-
-    test "when the anchor instance is in the middle of the string" do
-      assert Wildcard.match?("*.b.c.*", "a.b.c.d", @separator, @wildcard_char)
-      assert Wildcard.match?("a.*.b.c.*.d", "a.e.f.g.b.c.h.d", @separator, @wildcard_char)
-      refute Wildcard.match?("a.*.b.c.*.d", "a.e.f.g.b.c.d", @separator, @wildcard_char)
-    end
-
-    test "when the anchor instance is at the begining of the string" do
-      assert Wildcard.match?("a.b.c.*.d", "a.b.c.g.h.d", @separator, @wildcard_char)
-      refute Wildcard.match?("a.b.c.*.d", "a.b.f.e.d", @separator, @wildcard_char)
+    test "when the expr doesnt match" do
+      refute Wildcard.match?("aa.+", "bb.aa", @separator, @wildcard_char)
     end
   end
 
   describe "expr_valid?/1" do
-    test "when the expression is valid, returns true" do
+    test "when the expression is valid, return true" do
       assert Wildcard.expr_valid?("a.b.c", @separator, @wildcard_char)
     end
 
-    test "when the expression contains two adjacent separators, retuns false" do
-      refute Wildcard.expr_valid?("a..b", @separator, @wildcard_char)
-    end
-
-    test "when the expression is ended with a separator, returns false" do
-      refute Wildcard.expr_valid?("a.b.c.", @separator, @wildcard_char)
+    test "when the expression is invalid, retun false" do
+      refute Wildcard.expr_valid?("aa.", @separator, @wildcard_char)
+      refute Wildcard.expr_valid?("aa..bb", @separator, @wildcard_char)
+      refute Wildcard.expr_valid?(".aa", @separator, @wildcard_char)
+      refute Wildcard.expr_valid?("aa.+", @separator, @wildcard_char)
     end
   end
 
   describe "pattern_valid?/1" do
-    test "when the pattern is valid, returns true" do
-      assert Wildcard.pattern_valid?("a.b.c.*", @separator, @wildcard_char)
+    test "when the pattern is valid, return true" do
+      assert Wildcard.pattern_valid?("+.+", @separator)
+      assert Wildcard.pattern_valid?("aa.+.+", @separator)
+      assert Wildcard.pattern_valid?("+.+.aa", @separator)
+      assert Wildcard.pattern_valid?("a.b.c.+", @separator)
+      assert Wildcard.pattern_valid?("a.+.c.+", @separator)
     end
 
-    test "when the pattern is not valid_expr, returns false" do
-      refute Wildcard.pattern_valid?(".*.a", @separator, @wildcard_char)
-    end
-
-    test "when the pattern contains two adjacent wildcards, returns false" do
-      refute Wildcard.pattern_valid?("*.*", @separator, @wildcard_char)
-      refute Wildcard.pattern_valid?("a.*.*.b", @separator, @wildcard_char)
+    test "when the pattern is invalid, return false" do
+      refute Wildcard.pattern_valid?(".", @separator)
+      refute Wildcard.pattern_valid?("aa.", @separator)
+      refute Wildcard.pattern_valid?(".aa", @separator)
+      refute Wildcard.pattern_valid?("..aa", @separator)
+      refute Wildcard.pattern_valid?("aa..", @separator)
     end
   end
 end
