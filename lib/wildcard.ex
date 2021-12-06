c7ece18b8904be7469d3ff543f5c005cb89e58f0
MichaelTwito <20686573+MichaelTwito@users.noreply.github.com>
2021-12-06 20:01:04 +0200

    wildcard changed (#5)
    * wildcard changed
    
    * added testcases
    
    Co-authored-by: Michael Twito <michael.twito@annatel.net>

--- lib/wildcard.ex
+++ lib/wildcard.ex
@@ -1,20 +1,35 @@
 defmodule AntlUtilsElixir.Wildcard do
-  @spec match?(pattern :: binary, expr :: binary, separator :: binary, wildcard_char :: binary) ::
-          boolean
   def match?(pattern, expr, separator, wildcard_char)
-      when is_binary(pattern) and is_binary(expr) and is_binary(separator) and
-             is_binary(wildcard_char) do
-    pattern
-    |> String.replace(separator, "\\.")
-    |> String.replace(wildcard_char, ".+")
-    |> then(&("^" <> &1 <> "$"))
-    |> Regex.compile!()
-    |> Regex.match?(expr)
+      when is_binary(pattern) and is_binary(expr) do
+    with true <- pattern_valid?(pattern, separator),
+         true <- expr_valid?(expr, separator, wildcard_char),
+         true <- topics_number_equal?(pattern, expr, separator) do
+      pattern_as_list = String.split(pattern, separator)
+      expr_as_list = String.split(expr, separator)
+
+      Enum.zip(pattern_as_list, expr_as_list)
+      |> Enum.reduce(true, &(&2 and equal?(&1, wildcard_char)))
+    else
+      false -> false
+    end
   end
 
-  @spec pattern_valid?(binary, binary, binary) :: boolean
-  def pattern_valid?(pattern, separator, wildcard_char),
-    do: pattern_regex!(separator, wildcard_char) |> Regex.match?(pattern)
+  @spec pattern_valid?(binary, binary) :: boolean
+  def pattern_valid?(pattern, separator),
+    do: pattern_regex!(separator) |> Regex.match?(pattern)
+
+  defp topics_number_equal?(pattern, expr, separator),
+    do:
+      pattern |> String.graphemes() |> Enum.count(&(&1 == separator)) ==
+        expr |> String.graphemes() |> Enum.count(&(&1 == separator))
+
+  @spec pattern_regex!(binary) :: Regex.t()
+  defp pattern_regex!(separator) do
+    "^(?!\\#{separator})"
+    |> Kernel.<>("(?!.*" <> "?" <> "\\#{separator}\\#{separator}" <> ")")
+    |> Kernel.<>("(?!.*\\#{separator}$)")
+    |> Regex.compile!()
+  end
 
   @spec expr_valid?(binary, binary, binary) :: boolean
   def expr_valid?(expr, separator, wildcard_char),
@@ -29,12 +44,7 @@ defmodule AntlUtilsElixir.Wildcard do
     |> Regex.compile!()
   end
 
-  @spec pattern_regex!(binary, binary) :: Regex.t()
-  def pattern_regex!(separator, wildcard_char) do
-    "^(?!\\#{separator})"
-    |> Kernel.<>("(?!.*" <> "?" <> "\\#{separator}\\#{separator}" <> ")")
-    |> Kernel.<>("(?!.*" <> "\\#{wildcard_char}\\#{separator}\\#{wildcard_char}.*)")
-    |> Kernel.<>("(?!.*\\#{separator}$)")
-    |> Regex.compile!()
-  end
+  defp equal?({lhs, rhs}, wildcard_char) when lhs in [wildcard_char, rhs], do: true
+
+  defp equal?(_, _), do: false
 end
